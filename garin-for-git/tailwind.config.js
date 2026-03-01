e5c0ec39a72fea9fe7ea3f1a3e765f466513233a
ycshalom-ui <yc.shalom@gmail.com>
2026-03-01 09:18:30 +0200

    Add files via upload

---
+++ garin-for-git/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'blue-professional': '#166534',
+        'green-community': '#1e40af',
+      },
+      fontFamily: {
+        heebo: ['Heebo', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
